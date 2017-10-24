a2de8dace20ac26cf8d78aed8d35a09126ae2dae
bill00lee@hotmail.com <bill00lee@gmail.com>
2017-10-24 12:12:58 -0400

    secure

--- blog/templates/urls_2.py
+++ blog/templates/urls_2.py
@@ -20,8 +20,8 @@ from . import views
 
 
 urlpatterns = [
-        url(r'^admin/', admin.site.urls),
-        url(r'', include('blog.urls')),
+    url(r'^admin/', admin.site.urls),
+    url(r'', include('blog.urls')),
     url(r'^$', views.post_list, name='post_list'),
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 ]
--- mysite/settings.py
+++ mysite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 
+LOGIN_REDIRECT_URL = '/'
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -26,7 +27,6 @@ SECRET_KEY = 'x3svsg415ou$#=5qdcgyuf)tu99@2_ns7csptart3z57-&z$mn'
 DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'bill00lee.pythonanywhere.com']
-
 
 # Application definition
 
